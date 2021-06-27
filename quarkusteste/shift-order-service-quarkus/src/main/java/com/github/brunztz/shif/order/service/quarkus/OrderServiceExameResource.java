43f7cd5858ac5c4e3eaa4f131f13f90161f2da8d
BrunzTz <55399785+BrunzTz@users.noreply.github.com>
2021-06-27 19:39:43 -0300

    Delete quarkusteste/shift-order-service-quarkus directory

--- quarkusteste/shift-order-service-quarkus/src/main/java/com/github/brunztz/shif/order/service/quarkus/OrderServiceExameResource.java
+++
@@ -1,9 +0,0 @@
-package com.github.brunztz.shif.order.service.quarkus;
-
-import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
-import io.quarkus.rest.data.panache.ResourceProperties;
-
-@ResourceProperties(path = "api/order-service-exame")
-public interface OrderServiceExameResource extends PanacheEntityResource<OrderServiceExame, Long> {
-    
-}
--- quarkusteste/shift-order-service-quarkus/src/main/java/com/github/brunztz/shif/order/service/quarkus/PostoColetaResource.java
+++
@@ -1,9 +0,0 @@
-package com.github.brunztz.shif.order.service.quarkus;
-
-import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
-import io.quarkus.rest.data.panache.ResourceProperties;
-
-@ResourceProperties(path = "api/posto-coleta")
-public interface PostoColetaResource extends PanacheEntityResource<PostoColeta, Long> {
-    
-}
